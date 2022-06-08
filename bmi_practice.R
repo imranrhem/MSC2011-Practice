9694954d56cf340435dc18d782a3624c769cc2d6
imranrhem <imran.rhemtulla@gmail.com>
2022-06-07 20:17:50 -0400

    added error handling for negative input

--- bmi_practice.R
+++ bmi_practice.R
@@ -2,7 +2,7 @@
 
 #Get user input for height
 
-height <- readline(prompt = "What is your height in metres?")
+height <- readline(prompt = "What is your height in metres? ")
 
 #Convert height to a number
 
@@ -10,34 +10,42 @@ height <- as.numeric(height)
 
 #Check if user input is valid
 
-if (is.na(height)) {
+if (is.na(height) || height < 0) {
   print("Sorry, that is invalid. Exiting program.")
+} else {
+  
+  #Get user input for weight
+  
+  weight <- readline(prompt = "What is your weight in kilograms? ")
+  
+  #Convert weight to a number
+  
+  weight <- as.numeric(weight)
+  
+  #Check if user input is valid
+  
+  if (is.na(weight) || weight < 0) {
+    print("Sorry, that is invalid. Exiting program.")
+  } else {
+    
+    #Calculate user BMI
+    
+    bmi <- weight/(height^2)
+    
+    #Print result
+    
+    if (bmi < 18.5) {
+      print("You're in the underweight range.")
+    } else if (bmi <= 24.9) {
+      print("You're in the healthy weight range.")
+    } else if (bmi <= 29.9) {
+      print("You're in the overweight range.")
+    } else if (bmi <= 39.9) {
+      print("You're int the obese range.")
+    } else
+      print("Your BMI has not been categorized")
+    
+  }
+  
 }
 
-#Get user input for weight
-
-weight <- readline(prompt = "What is your weight in kilograms?")
-
-#Convert weight to a number
-
-weight <- as.numeric(weight)
-
-#Check if user input is valid
-
-if (is.na(weight)) {
-  print("Sorry, that is invalid. Exiting program.")
-}
-
-#Calculate user BMI
-
-bmi <- weight/(height^2)
-
-if (bmi < 18.5) {
-  print("You're in the underweight range.")
-} else if (bmi <= 24.9) {
-  print("You're in the healthy weight range.")
-} else if (bmi <= 29.9) {
-  print("You're in the overweight range.")
-} else if (bmi <= 39.9) {
-  print("You're int the obese range.")
-}
