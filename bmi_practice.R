379b80a3436a807096d3eefa909112737a7839ee
imranrhem <imran.rhemtulla@gmail.com>
2022-06-07 20:11:55 -0400

    creation of the BMI calculator

---
+++ bmi_practice.R
@@ -0,0 +1,43 @@
+#BMI Calculator
+
+#Get user input for height
+
+height <- readline(prompt = "What is your height in metres?")
+
+#Convert height to a number
+
+height <- as.numeric(height)
+
+#Check if user input is valid
+
+if (is.na(height)) {
+  print("Sorry, that is invalid. Exiting program.")
+}
+
+#Get user input for weight
+
+weight <- readline(prompt = "What is weight in kilograms?")
+
+#Convert weight to a number
+
+weight <- as.numeric(weight)
+
+#Check if user input is valid
+
+if (is.na(weight)) {
+  print("Sorry, that is invalid. Exiting program.")
+}
+
+#Calculate user BMI
+
+bmi <- weight/(height^2)
+
+if (bmi < 18.5) {
+  print("You're in the underweight range.")
+} else if (bmi <= 24.9) {
+  print("You're in the healthy weight range.")
+} else if (bmi <= 29.9) {
+  print("You're in the overweight range.")
+} else if (bmi <= 39.9) {
+  print("You're int the obese range.")
+}
